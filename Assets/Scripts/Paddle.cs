d58c09e5892dd6a16968bdd84845191f606b24dc
natxoraga22 <natxoraga22@gmail.com>
2017-04-23 04:01:09 +0200

    Extended paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
+	public float minXPosition = 1f;
+	public float maxXPosition = 15f;
+
 	public bool autoPlay = false;
 	private Ball ball;
 
@@ -23,14 +26,14 @@ public class Paddle : MonoBehaviour {
 	void MoveWithMouse () {
 		// Make the paddle follow the mouse X position
 		float mouseXPosition = (Input.mousePosition.x / Screen.width) * 16;
-		Vector3 paddlePosition = new Vector3(Mathf.Clamp(mouseXPosition, 0.5f, 15.5f), this.transform.position.y, this.transform.position.z);
+		Vector3 paddlePosition = new Vector3(Mathf.Clamp(mouseXPosition, minXPosition, maxXPosition), this.transform.position.y, this.transform.position.z);
 		this.transform.position = paddlePosition;
 	}
 
 	void AutoPlay () {
 		// Paddle always below the ball
 		float ballXPosition = ball.transform.position.x;
-		Vector3 paddlePosition = new Vector3(Mathf.Clamp(ballXPosition, 0.5f, 15.5f), this.transform.position.y, this.transform.position.z);
+		Vector3 paddlePosition = new Vector3(Mathf.Clamp(ballXPosition, minXPosition, maxXPosition), this.transform.position.y, this.transform.position.z);
 		this.transform.position = paddlePosition;
 	}
 
